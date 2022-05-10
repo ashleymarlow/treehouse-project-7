0bb4748168231d41cff909a361f4fa96819011b3
Ashley Marlow <ashley.marlow@hotmail.com>
2022-05-09 18:14:01 -0700

    desktop finished

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,19 @@
 let icon = document.getElementById("exit-icon");
 let alertBar = document.getElementById("alert-bar");
-icon.addEventListener('click', function handleClick() {
+icon.addEventListener('click', function() {
     alertBar.classList.add('hidden');
 });
+
+let messageForm = document.getElementById("message-form");
+messageForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let user = document.getElementById("user").value;
+    let message = document.getElementById("message").value;
+    if (user == '' || message == '') {
+        alert('Error: Empty Input');
+    } else {
+        alert('Message Sent!');
+    }
+
+});
+
